438a56d40c9b52ecedb175dbdc3675553dae6f99
KRISHNA KUMAR SHARMA <102216464+Kingkrishna889@users.noreply.github.com>
2023-08-23 00:05:57 +0530

    Added Exercise 1_1

---
+++ Weeek 1/Exercise 1_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;  
+public class Exercise1_1 {
+       public static void main(String[] args) {
+Scanner s = new Scanner(System.in); 
+       double radius= s.nextDouble();
+       double perimeter;
+       double area;
+//Calculate the perimeter
+    
+	perimeter = 2 * Math.PI * radius;
+
+//Calculate the area
+	
+	area = Math.PI* radius * radius;
+	
+	System.out.println(perimeter);
+	System.out.print(area);
+}
+}
